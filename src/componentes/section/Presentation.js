0005a9523492696c308745141f75950da0d9c92b
Daniele Benfica <103818625+danibenfica@users.noreply.github.com>
2023-08-08 22:03:24 -0300

    versao02

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,12 +1,44 @@
 import ButtonA from '../elements/ButtonA'
-import ButtonB from '../elements/ButtonB'
 import styles from './Presentation.module.css'
+import { useEffect, useState } from 'react'
 
 function Presentation() {
+
+    const [text, setText] = useState('')
+    const toRotate = ['Daniele Benfica', 'QA', 'Desenvolvedora Front-End']
+    const [loop, setLoop] = useState(0)
+    const [isDeleting, setIsDeleting] = useState(false)
+    const period = 100
+    const [delta, setDelta] = useState(100)
+
+    useEffect(()=> {
+        let ticker = setInterval(()=> {
+            toType()
+        }, delta)
+        return()=>{clearInterval(ticker)}
+    }, [text])
+
+    const toType = () => {
+        let i = loop % toRotate.length
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+
+        setText(updatedText)
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true)
+            setDelta(period)
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false)
+            setDelta(period)
+            setLoop(loop+1)
+        }
+    }
+
     return(
         <div className={styles.presentation} id="Presentation">
             <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
-            <h1>Olá, eu sou Daniele Benfica!</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p>
                 Trabalho como QA, realizando testes funcionais e de usabilidade para analisar a qualidade do software. <br/>
                 Além disso, proponho melhorias com base nas minhas descobertas e identifico possíveis bugs. <br/>
